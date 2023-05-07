2c4ff766b96d223cf0b3ff0137858f74f399aa04
jorgeDynamite <tobiesongeorge@gmail.com>
2023-05-07 10:59:48 +0200

    first commit

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM python:3.9
 
 ADD python.py .
 
+RUN pip install scikit-learn
+
 CMD ["python", "./python.py"]
--- python.py
+++ python.py
@@ -1,1 +1,20 @@
+import time
+import socket 
+import sklearn.datasets import load_iris
+
+data = load_iris()
 print("Hello World")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", 9999))
+
+server.listen()
+
+while True: 
+    client, addr = server.accept()
+    print("Connection from", addr)
+    client.send("You are connected\n".encode())
+    client.send(f"{data[0][:,0]}\n".encode())
+    time.sleep(2)
+    client.send("Dissconnect\n".encode())
+    client.close()
