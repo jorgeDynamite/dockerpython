c9213f855f627afe68436d781f58061fa4cafa22
jorgeDynamite <tobiesongeorge@gmail.com>
2023-05-07 11:54:52 +0200

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.9
 ADD main.py .
-RUN pip install --index-url=https://pypi.python.org/simple/ scikit-learn
+RUN pip install --index-url=https://pypi.python.org/simple/ tensorflow
 CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import time
 import socket
-from sklearn.datasets import load_iris
+import tensorflow as tf
 
+
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", 9999))
+
+server.listen()
+
+while True:
+    client, addr = server.accept()
+    print("Connection from", addr)
+    client.send("You are connected\n".encode())
+    client.send(f"{tf.__version__}".encode())
+    print("1")
+    time.sleep(2)
+    client.send("Dissconnect\n".encode())
+    print("2")
+    client.close()
+"""
+
+
+"""
+from sklearn.datasets import load_iris
 data = load_iris()
 
 
@@ -20,3 +43,4 @@ while True:
     client.send("Dissconnect\n".encode())
     print("2")
     client.close()
+"""
